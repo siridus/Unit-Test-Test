c37d39b1e3290125144c27d6539df51592db25c6
siridus <43641086+siridus@users.noreply.github.com>
2019-06-03 11:49:27 +0200

    Removed Unit Tests and creates our own

--- src/app/car.spec.ts
+++ src/app/car.spec.ts
@@ -1,7 +1,30 @@
 import { Car } from './car';
 
+
+let car: Car;
+
+beforeEach(() => {
+  car = new Car('Test', 10, 'Beschreibung');
+});
+
+afterEach(() => {
+  car = null;
+});
+
 describe('Car', () => {
   it('should create an instance', () => {
-    expect(new Car('Test', 10, 'Beschreibung')).toBeTruthy();
+    expect(car).toBeTruthy();
+  });
+
+  it('should return type "Test"', () => {
+    expect(car.getType()).toBe('Test');
+  });
+
+  it('should return type "10"', () => {
+    expect(car.getValue()).toBe(10);
+  });
+
+  it('should return type "Test"', () => {
+    expect(car.getDescription()).toBe('Beschreibung');
   });
 });
